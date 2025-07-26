31193ab6bcc79b0f73727f68759fc0444c62c639
MenaceHecker <thedynamic2508@gmail.com>
2025-07-25 23:44:06 -0400

    Added useRefs

--- src/sections/ShowcaseSection.tsx
+++ src/sections/ShowcaseSection.tsx
@@ -6,11 +6,11 @@ const ShowcaseSection = () => {
     const project2Ref = useRef(null); 
     const project3Ref = useRef(null); 
   return (
-    <section id="work"className='app-showcase'>
+    <section id="work" ref={sectionRef} className='app-showcase'>
         <div className='w-full'>
             <div className='showcaselayout'>
                 {/* LEFT SIDE */}
-                <div className="first-project-wrapper">
+                <div className="first-project-wrapper" ref={project1Ref}>
                     <div className="image-wrapper">
                         <img src="/images/project1.png" alt="crumb" />
                     </div>
@@ -23,13 +23,13 @@ const ShowcaseSection = () => {
                 </div>
                 {/* RIGHT SIDE */}
                 <div className="project-list-wrapper overflow-hidden">
-                    <div className="project">
+                    <div className="project" ref={project2Ref}>
                         <div className="image-wrapper bg-[#d5d0c8]">
                         <img src="/images/project2.png" alt="Email Organizer" />
                         </div>
                         <h2>Smart Email Organizer</h2>
                     </div>
-                    <div className="project">
+                    <div className="project" ref={project3Ref}>
                         <div className="image-wrapper bg-[#d5d0c8]">
                         <img src="/images/project3.png" alt="Email Organizer" />
                         </div>
