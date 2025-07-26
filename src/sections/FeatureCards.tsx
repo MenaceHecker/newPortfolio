d6a85de6a17c1ebebef85f2c421115e43dd7f39c
MenaceHecker <thedynamic2508@gmail.com>
2025-07-26 18:04:42 -0400

    Adding qualities to FeatureCards

--- src/sections/FeatureCards.tsx
+++ src/sections/FeatureCards.tsx
@@ -7,7 +7,10 @@ const FeatureCards = () => {
     <div className='w-full paddinx-g-lg'>
         <div className='mx-auto grid-3-col'>
             {abilities.map (({imgPath, title, desc}) => (
-                <div>
+                <div key={title} className='card-border rounded-xl p-8 flex flex-col gap-4'>
+                    <div className='size-14 flex items-center justify-center rounded-full'>
+                        <img src={imgPath} alt={title} />
+                    </div>
                     <h3>{title}</h3>
                 </div>
             ))}
