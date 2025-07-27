7e5794214dea89a9291a37a5ba2eb319ca853e59
MenaceHecker <thedynamic2508@gmail.com>
2025-07-27 16:13:21 -0400

    Working on exp cards now

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -11,14 +11,27 @@ const ExperienceSection = () => {
             sub=" 💼 My Career Overview"/>
             <div className="mt-32 relative">
               <div className="relative z-50 xl:space-y-32 space-y-10">
-                {expCards.map((card) => (
+                {expCards.map((card, index) => (
                   <div key={card.title} className="exp-card-wrapper">
                     <div className="xl:w-2/6">
-                    <GlowCard card={card}>
+                    <GlowCard card={card} index={index}>
                       <div>
                         <img src={card.imgPath} alt={card.title} />
-                      </div>
+                      </div> 
                     </GlowCard>
+                    </div>
+                    <div className="xl:w-4/6 ">
+                    <div className="flex items-start">
+                      <div className="timeline-wrapper">
+                        <div className="timeline"/>
+                        <div className="gradiet-line w-1 h-full"/>
+                      </div>
+                      <div className="expText flex xl:gap-20 md: gap-10 gap-5 relative z-20">
+                        <div className="timeline-logo">
+                          <img src={card.logoPath} alt="logo" />
+                        </div>
+                      </div>
+                    </div>
                     </div>
                     </div>
                 ))}
