1a856bf49adfdcce580399fd026cf5452fa9fde0
MenaceHecker <thedynamic2508@gmail.com>
2025-07-27 14:53:47 -0400

    Added some visual stuff in GlowCard

--- src/components/GlowCard.tsx
+++ src/components/GlowCard.tsx
@@ -1,6 +1,14 @@
-const GlowCard = () => {
+const GlowCard = ({card, children}) => {
   return (
-    <div>GlowCard</div>
+    <div className="card card-border timeline-card rounded-xl p-10">
+        <div className="glow">
+            <div>
+                {Array.from({length: 5}, (_, i) => (
+                    <img src="/images/star.png" key ={i} alt="star" className="star" />
+                ))}
+            </div>
+        </div>
+    </div>
   )
 }
 
