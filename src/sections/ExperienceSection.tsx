a6ab1719c9ae37b17a8394f0d10d37e2565ad0e1
MenaceHecker <thedynamic2508@gmail.com>
2025-07-27 14:13:47 -0400

    Working on experience cards

--- src/sections/ExperienceSection.tsx
+++ src/sections/ExperienceSection.tsx
@@ -1,4 +1,5 @@
 import TitleHeader from "../components/TitleHeader"
+import { expCards } from "../constants"
 
 const ExperienceSection = () => {
   return (
@@ -7,6 +8,15 @@ const ExperienceSection = () => {
             <TitleHeader 
             title="Professional Work Experience" 
             sub=" 💼 My Career Overview"/>
+            <div className="mt-32 relative">
+              <div className="relative z-50 xl:space-y-32 space-y-10">
+                {expCards.map((card) => (
+                  <div key={card.title} className="exp-card-wrapper">
+                    {card.title}
+                    </div>
+                ))}
+              </div>
+            </div>
         </div>
     </section>
   )
