2fa19ca0fc17f5af1054e1d2cd16313931e4cc07
MenaceHecker <thedynamic2508@gmail.com>
2025-07-28 10:44:25 -0400

    Added new content to TechStack

---
+++ src/sections/TechStack.tsx
@@ -0,0 +1,16 @@
+import TitleHeader from "../components/TitleHeader"
+
+const TechStack = () => {
+  return (
+    <div id="skills" className="flex-center section-padding">
+        <div className="w-full h-full md:px-10 px-5">
+            <TitleHeader
+            title="My Favorite Tech Stack"
+            sub="The skills I bring to the Table"/>
+
+        </div>
+    </div>
+  )
+}
+
+export default TechStack
