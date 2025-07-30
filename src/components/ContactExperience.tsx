1ba9a7b00e4b270465e8c44bf02c877f616aff9c
MenaceHecker <thedynamic2508@gmail.com>
2025-07-30 17:43:25 -0400

    Added spotlight

--- src/components/ContactExperience.tsx
+++ src/components/ContactExperience.tsx
@@ -7,6 +7,13 @@ const ContactExperience = () => {
     <Canvas camera={{position: [0,3,7], fov: 45}} shadows>
       <ambientLight intensity={0.5} />
       <directionalLight position={[5,5,5]} intensity={1}/>
+      <spotLight
+      position={[-5,5,5]}
+      angle={0.15}
+      penumbra={1}
+      intensity={1}
+      castShadow
+      />
     </Canvas>
   )
 }
