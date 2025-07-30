1b43bc5e647feebf569fe86524b35967dd704ba1
MenaceHecker <thedynamic2508@gmail.com>
2025-07-30 17:40:29 -0400

    Made some changes

--- src/components/ContactExperience.tsx
+++ src/components/ContactExperience.tsx
@@ -1,3 +1,7 @@
+import { Environment, Float, OrbitControls} from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+import * as THREE from 'three'
+
 const ContactExperience = () => {
   return (
     <div>ContactExperience</div>
