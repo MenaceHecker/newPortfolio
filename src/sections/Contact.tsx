2dc0dde42652039bfe007c1a9d6b602c9b110c06
MenaceHecker <thedynamic2508@gmail.com>
2025-07-30 14:55:30 -0400

    Added type import for ChangeEvent

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,22 +1,41 @@
-import { useState } from 'react'
+import { useState} from 'react'
+import type { ChangeEvent } from 'react';
 import TitleHeader from '../components/TitleHeader'
 
+
+
+//When verbatimModuleSyntax is enabled in the TypeScript config, then use type-only imports for types that are only used for type annotations
+interface FormData {
+    name: string;
+    email: string;
+    message: string;
+}
+
 const Contact = () => {
-    const [formData, setFormData] = useState({
+    const [formData, setFormData] = useState<FormData>({
         name: '',
         email: '',
         message: ''
-    })
-  return (
-    <section id='contact' className='flex-center section-padding'>
-        <div className='w-full h-full md:px-10 px-5'>
-            <TitleHeader
-            title="Let's connect"
-            sub='Contact Information'
-            />
-        </div>
-    </section>
-  )
+    }); 
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target; 
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    return (
+        <section id='contact' className='flex-center section-padding'>
+            <div className='w-full h-full md:px-10 px-5'>
+                <TitleHeader
+                    title="Let's connect"
+                    sub='Contact Information'
+                />
+            </div>
+        </section>
+    )
 }
 
 export default Contact
