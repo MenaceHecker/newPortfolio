6b5d128396c2f2905e9c1ad3209124e60c07801a
MenaceHecker <thedynamic2508@gmail.com>
2025-07-30 14:41:07 -0400

    Created and working on contact section

---
+++ src/sections/Contact.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+
+const Contact = () => {
+  return (
+    <section id='contact' className='flex-center section-padding'>
+        <div className='w-full h-full md:px-10 px-5'>
+            <TitleHeader
+            title="Let's connect"
+            sub='Contact Information'
+            />
+        </div>
+    </section>
+  )
+}
+
+export default Contact
