9855e066083a05bf13038c2da964187750b9497c
MenaceHecker <thedynamic2508@gmail.com>
2025-08-02 13:13:59 -0400

    Added android logo but not decent scaling

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -138,10 +138,10 @@ const techStackIcons = [
     rotation: [0, Math.PI * 0.1, 0],
   },
   {
-    name: "Project Manager",
-    modelPath: "/models/git-svg-transformed.glb",
-    scale: 0.05,
-    rotation: [0, -Math.PI / 4, 0],
+    name: "Android Developer",
+    modelPath: "/models/android_logo.glb",
+    scale: 0.8,
+    rotation: [0,0, 0],
   },
 ];
 
